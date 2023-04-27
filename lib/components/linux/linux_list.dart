0fc1dd2bfa39e06f458c4025325e2cab24cae41a
bobdonald <bobdonald@pop-os.localdomain>
2023-04-27 11:58:35 -0600

    add linux and vim scommands

---
+++ lib/components/linux/linux_list.dart
@@ -0,0 +1,4 @@
+const Map<String, String> words = {
+	'ls': 'Lists all files and directories in the present working directory',
+	'ls -R': 'Lists all files and directories, and files in sub-directories as well',
+}
