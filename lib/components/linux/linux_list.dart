b387cead0db21cdba2592a4b12b982a7ddcc1b71
Robert812218 <robert812@shaw.ca>
2023-04-27 12:21:34 -0600

    update

--- lib/components/linux/linux_list.dart
+++ lib/components/linux/linux_list.dart
@@ -1,4 +1,5 @@
 const Map<String, String> words = {
 	'ls': 'Lists all files and directories in the present working directory',
 	'ls -R': 'Lists all files and directories, and files in sub-directories as well',
-}
+};
+
---
+++ lib/components/vim/vim_list.dart
@@ -0,0 +1,4 @@
+const Map<String, String> words = {
+	'vim word: ': 'vim description',
+	'vim word': 'vim description',
+}
